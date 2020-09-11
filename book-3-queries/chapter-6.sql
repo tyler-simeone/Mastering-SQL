09d38aa18a586010b5e02f5cd1ee306a1949b154
Tyler Simeone <tyler.simeone@gmail.com>
2020-09-11 15:26:02 -0500

    working on question 2 in chapter 6

--- book-3-queries/chapter-6.sql
+++ book-3-queries/chapter-6.sql
@@ -33,3 +33,41 @@ where dealership_id = 1000;
 
 select * from dealerships 
 order by dealership_id desc;
+
+
+-- 2.
+
+select * from dealerships
+order by dealership_id desc;
+
+create or replace function default_phone_num()
+	returns trigger
+	language plpgsql
+as $$
+begin
+
+-- 	if new.phone is null then
+-- 		new.phone = '777-111-0305';
+-- 	end if;
+		
+-- 	return new;
+
+	update dealerships
+	set phone = '777-111-0305'
+	where new.phone is null;
+	
+	return null;
+
+end;
+$$
+
+create trigger add_phone
+	after insert
+	on dealerships
+	for each row
+	execute procedure default_phone_num();
+	
+-- Test
+
+insert into dealerships(business_name)
+values('New dealership 3!');

1f8615c7a4804172a2d780d64a216d57bc3973c1
Tyler Simeone <tyler.simeone@gmail.com>
2020-09-25 11:52:10 -0500

    final group project complete

--- book-4-queries/group-project.sql
+++ book-4-queries/group-project.sql
@@ -57,3 +57,68 @@ JOIN denormvehicletypes dvt
 ON v.vehicle_type_id = dvt.vehicle_type_id
 WHERE v.msr_price > 15000
 ORDER BY msr_price;
+
+
+
+-- TYLER'S SQL QUERIES!
+
+
+CREATE OR REPLACE FUNCTION fix_pickup_date()
+	RETURNS TRIGGER
+	LANGUAGE plpgsql
+AS $$
+BEGIN
+	
+	UPDATE sales
+	SET pickup_date = purchase_date + integer '7'
+	WHERE sales.pickup_date <= sales.purchase_date;
+	
+	UPDATE sales
+	SET pickup_date = pickup_date + integer '4'
+	WHERE sales.pickup_date < sales.purchase_date + integer '7';
+	
+	RETURN null;
+	
+END;
+$$
+
+CREATE TRIGGER update_pickup_date
+	AFTER INSERT OR UPDATE
+	ON sales
+	FOR EACH ROW
+	EXECUTE PROCEDURE fix_pickup_date();
+
+
+
+
+-- Create stored proc to sell a vehicle (common use case)
+
+CREATE OR REPLACE PROCEDURE sell_vehicle(saleTypeId int, vehicleId int, employeeId int, 
+										 customerId int, dealershipId int, price numeric, deposit int, 
+										 purchaseDate date, pickupDate date, invoiceNum varchar(50), 
+										 paymentMethod varchar(50))
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+	-- Making sure vehicle hasn't been sold, and if so, making sure vehicle has been returned
+	IF NOT EXISTS (SELECT * FROM sales WHERE vehicle_id = vehicleId AND returned = false) THEN
+	  
+		INSERT INTO sales (sales_type_id, vehicle_id, employee_id, customer_id, 
+						   dealership_id, price, deposit, purchase_date, pickup_date,
+						   invoice_number, payment_method)
+		VALUES (saleTypeId, vehicleId, employeeId, customerId, dealershipId,
+				price, deposit, purchaseDate, pickupDate, invoiceNum, paymentMethod);
+				
+	END IF;
+				
+END;
+$$
+
+SELECT * FROM sales 
+ORDER BY sale_id DESC;
+
+CALL sell_vehicle(1, 255, 25, 126, 100, 79999.49, 10000, '2020-09-23', '2020-09-30', '213V7W9854', 'mastercard');
+CALL sell_vehicle(1, 50, 25, 125, 100, 31976.59, 10000, '2020-09-23', '2020-09-30', '423V7X9784', 'visa');
+
+
